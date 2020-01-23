213e47bf59d2d7b76d0b1b02464e926e3e46b891
AinaMontalban <aina.montalban@alum.esci.upf.edu>
2020-01-23 15:44:14 +0100

    new file

---
+++ plotPCA.R
@@ -0,0 +1,38 @@
+plotPCA <- function(pca, pc1, pc2, col, covariates, selectedCov, bty="o"){
+    require(plotly)
+    
+    pc1pc2 <- cbind(pca[,as.numeric(pc1)], pca[,as.numeric(pc2)])
+                    
+    ax_x <- list(
+        title = paste("PC", as.numeric(pc1)),
+        zeroline = FALSE,
+        showline = TRUE,
+        showticklabels = TRUE,
+        showgrid = TRUE
+    )
+    
+    ax_y <- list(
+        title = paste("PC", as.numeric(pc2)),
+        zeroline = FALSE,
+        showline = TRUE,
+        showticklabels = TRUE,
+        showgrid = TRUE
+    )
+    
+
+    plot_ly(as.data.frame(pc1pc2),x=~V1, y=~V2, type = "scatter", mode="markers", text="", color = unique(covariates[,match(selectedCov, colnames(covariates))]), size = 2) %>%  
+        layout(xaxis = ax_x, yaxis = ax_y)
+    
+    #plot(pca[,as.numeric(pc1)], pca[,as.numeric(pc2)],
+     #    col = col, pch = 19, cex = 2, xlab = xlab,
+     #    ylab = ylab, xlim = xlim,
+     #    main = "Principal component analysis (PCA)",
+     #    cex.main = 1.5, cex.lab = 1.5, bty = bty)
+    #uColor <- unique(col)
+    #uCov   <- unique(covariates[,match(selectedCov, colnames(covariates))])
+    
+    #legend("bottomright", legend = uCov, pch = 19, col = uColor,
+       #    cex = 1.5, title = selectedCov, bty = "n")
+    #grid()
+}
+
---
+++ runshiny.R
@@ -0,0 +1,85 @@
+# Aina Montalban
+require(knitr)
+require(limma)
+require(minfi)
+#require(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+#require(IlluminaHumanMethylation450kmanifest)
+require(IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
+require(RColorBrewer)
+require(missMethyl)
+require(Gviz)
+#require(DMRcate)
+require(stringr)
+require(ggplot2)
+require(shiny)
+require(base)
+require(reshape2)
+# Set the directories where the data is stored
+# Full path of the folder
+full_path <- "/mnt/ElRaid/amontalban/PROJECTS/metylation/subset"
+# Path of the IDAT files
+directory.IDAT<- file.path(full_path, "tmp", fsep = "/")
+# Path of the samplesheet file
+directory.targets <- file.path(full_path, "data", fsep = "/")
+print(paste(directory.targets))
+# Directory to save the results
+directory.results <- file.path(full_path, "aina/results", fsep = "/")
+
+#===================#
+# A) Read targets
+#===================#
+targets <- read.metharray.sheet(directory.targets, pattern = "sample_sheet_PDX_subset.csv")
+
+# Create a column called Bassename which specifies the location of each individual IDAT file
+# in the experiment
+targets$Basename <- file.path(directory.IDAT, paste(targets$Slide, targets$Array, sep = "_"))
+
+# Convert Sample_Name as character.
+targets$Sample_Name <- as.character(targets$Sample_Name)
+# Print how many samples do we have
+print(paste("Full sample sheet rows (samples):", nrow(targets)))
+
+# Remove samples without responder info.
+targets <- targets[targets$Sample_Group != "", ]
+print(paste("Sample sheet rows (samples) after removing samples w/o info:", 
+            nrow(targets)))
+
+
+#===================#
+# B) Raw data
+#===================#
+
+# Read raw data
+rgSet <- read.metharray.exp(targets=targets, force = TRUE)
+# Give the samples descriptive names
+targets$ID <- paste(targets$Slide,targets$Array, targets$Sample_Name,targets$Sample_Group, sep=".")
+# Define the sample names into the rgSet
+sampleNames(rgSet) <- targets$ID
+# print
+rgSet
+
+
+directory <- "~/Documents/shiny01/shinyHome01"
+directory <- "/home/aina/Internship/shinyHome01"
+source(paste0(directory, "/", "MethylationShiny00.R"))
+source(paste0(directory, "/", "shinyMethylSet.R"))
+source(paste0(directory, "/", "shinySummarize0.R"))
+source(paste0(directory, "/", "plotDensities.R"))
+source(paste0(directory, "/", "plotPCA.R"))
+source(paste0(directory, "/", "plotPropFailedProbes.R"))
+
+
+
+
+baseDir <- system.file("extdata", package="minfiData")
+targetsEx <- read.metharray.sheet(baseDir)
+RGSetEx <- read.metharray.exp(targets = targetsEx)
+summary1 <- shinySummarize(RGSetEx)
+targetsEx$ID <- paste(targetsEx$Slide,targetsEx$Array, targetsEx$Sample_Name,targetsEx$Sample_Group, sep=".")
+summary <- shinySummarize(rgSet)
+mSetSq <- preprocessQuantile(RGSetEx)
+summary1.norm <- shinySummarize(mSetSq)
+
+shinyApp(ui=ui.methylation(summary1), server = server.methylation(summary1))
+shinyApp(ui=ui.methylation(summary1,summary1.norm ), server = server.methylation(summary1, summary1.norm))
+  
